7c9f8d3ef9e2eac36c68ff24133213ce6603b9ef
Josh Payne <59780902+Josh-BI-UK@users.noreply.github.com>
2023-04-14 22:57:40 +0100

    Adds logic to calculate years, months, days lived to test.py

--- Essential_GitHub_Lessons/test.py
+++ Essential_GitHub_Lessons/test.py
@@ -1,13 +1,23 @@
 from datetime import timedelta, date
 import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 hrs_in_day = 24
 dt_today = date.today() 
 dt_birthday = dt.date(1987,2,17)
 
+delta_today_bday = relativedelta(dt_today, dt_birthday)
+
 days_from_birthday = dt_today - dt_birthday
 
-print(days_from_birthday.days)
-hrs_lived = (days_from_birthday.days * hrs_in_day)
 
-print(hrs_lived)
+yrs_lived = delta_today_bday.years
+mths_lived = yrs_lived * 12
+days_lived = days_from_birthday.days
+hrs_lived = (days_lived * hrs_in_day)
+
+print(f"Hours Lived: {hrs_lived:,}\n")
+print(f"Days Lived: {days_lived:,}\n")
+print(f"Months Lived: {mths_lived:,}\n")
+print(f"Years Lived: {yrs_lived:,}\n")
+print(f"Life Time: {delta_today_bday.years} years, {delta_today_bday.months} months, {delta_today_bday.days} days\n")
